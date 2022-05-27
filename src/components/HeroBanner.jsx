d472c2a016b39731d0bd63cde65cf0253bef3288
javier marquez <hello.jmarquez@gmail.com>
2022-05-27 11:44:02 -0500

    files added

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const HeroBanner = ({ img, data }) => {
+	let [counter, setCounter] = useState(0);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCounter(counter => counter + 1);
+			if (counter > 4) setCounter(0);
+		}, 4000);
+		return () => clearInterval(timer);
+	}, []);
+
+	return (
+		<div>
+			<div>{data.length > 0 && data[0].original_title}</div>
+			{data.length > 0 && (
+				<img
+					src={data.length > 0 && `${img}${data[counter].poster_path}`}
+					alt={data.original_title || data.original_name}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default HeroBanner;
